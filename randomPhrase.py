dc74a0b4c9d8198cae00f899f19b392a19524b5d
Joshua <Joshua4yala@pm.me>
2020-10-31 00:03:24 +0100

    Create randomPhrase.py

---
+++ randomPhrase.py
@@ -0,0 +1,40 @@
+ # coding=utf-8
+#by Joshua
+
+import random
+
+i = 0;
+x = random.randint(0, 10)
+
+if(x==1):
+	print("Felicidad no es hacer lo que uno quiere sino querer lo que uno hace.")
+	print("													 (Jean Paul Sartre)")
+elif(x==2):
+	print("La felicidad no es un ideal de la razón, sino de la imaginación.")
+	print("													(Immanuel Kant)")
+elif(x==3):
+	print("No hay hombre tan cobarde a quien el amor no haga valiente y transforme en héroe.")
+	print("																			(Platón)")
+elif(x==4):
+	print("Todo lo que se hace por amor, se hace más allá del bien y del mal.")
+	print("										(Friedrich Wilhelm Nietzsche)")
+elif(x==5):
+	print("Hay alguien tan inteligente que aprende de la experiencia de los demás.")
+	print("																(Voltaire)")
+elif(x==6):
+	print("No puedo enseñar nada a nadie. Solo puedo hacerles pensar.")
+	print("													(Sócrates)")
+elif(x==7):
+	print("No lastimes a los demás con lo que te causa dolor a ti mismo.")
+	print("														(Buda)")
+elif(x==8):
+	print("El corazón tiene razones que la razón ignora.")
+	print("								(Blaise Pascal)")
+elif(x==9):
+	print("El deseo es la verdadera esencia del hombre.")
+	print("									(Spinoza)")
+elif(x==10):
+	print("Vivir sin filosofar es, propiamente, tener los ojos cerrados, sin tratar de abrirlos jamás.")
+	print("																			(René Descartes)")
+else:
+	print("Error.")
